6090b6495ec74914f29a0ae4975adec5ac451707
Sean Coffey <seanecoffey@hotmail.com>
2017-02-25 22:48:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -43,6 +43,10 @@ app.get('/callback', function(req, res) {
     });
 });
 
+app.get('/refresh', function(req,res) {
+  spotifyApi.refreshAccessToken()
+}
+
 app.use('/store', function(req, res, next) {
   if (req.body.token !== process.env.SLACK_TOKEN) {
     return res.status(500).send('Cross site request forgerizzle!');
